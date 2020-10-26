40a5a611ce1bace27cef5faa9cf06a8da4e93abd
vivacious-revert-7013 <mesmith454@gmail.com>
2020-10-26 18:34:26 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Debugging with Pry
+b # Debugging with Pry
 
 ## Learning Goals
 
--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,15 +1,22 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
 
-def state_log(star_date)
-  "Captain's Log, star date #{star_date}."
+def state_log(generate_star_date)
+  "Captain's Log, star date #{generate_star_date}."
+end
+
+def crew
+  ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
+end
+
+def greet_crew (crew)
+  crew.each {|crew_member| "Hello #{crew_member}."}
 end
 
 def engage
-  puts state_log(date)
-  date = generate_star_date
+  return state_log(generate_star_date) + greet_crew(crew)
 end
